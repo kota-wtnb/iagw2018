fc0f096ab60e8ed020006438ec92362017b6c2a9
yamajyn <j.yama.japan@gmail.com>
2018-01-14 14:52:37 +0900

    headerの追加

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
 $(function(){
-  $(window).scroll(function() {
-  var scrollPos = $(window).scrollTop();
   var height = window.innerHeight;
-    setAnimation($('#about'),scrollPos,0,height);
-    setAnimation($('#concept'),scrollPos,height,height*2);
-    setAnimation($('#info'),scrollPos,height*2,height*3);
+  var conceptPos = height;
+  var infoPos = height * 2;
+  var exhibitPos = height * 3;
+  $(window).scroll(function() {
+    var scrollPos = $(window).scrollTop();
+    setAnimation($('#about'),scrollPos,0,conceptPos);
+    setAnimation($('#concept'),scrollPos,conceptPos,infoPos);
+    setAnimation($('#info'),scrollPos,infoPos,exhibitPos);
+    $('#nav').css({'top':scrollPos/height/5 * $('header').height()});
+  });
+  
+  $('header a').click(function() {
+    var speed = 3000;
+    var target= $(this).attr("href");
+    var position = 0;
+    switch (target) {
+      case "#about":
+        position = 0;
+        break;
+      case "#concept":
+        position = conceptPos + height * 2/3;
+        break;
+      case "#info":
+        position = infoPos + height * 2/3;
+        break;
+      default:
+        position = 0;
+    }
+     $('body,html').animate({scrollTop:position}, speed, 'swing');
+     return false;
   });
 });
 
